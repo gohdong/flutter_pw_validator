679313f57cc1dcf129c6c034c3c3aa31a119443a
gohdong <aldehf420@naver.com>
2021-04-02 02:04:50 +0900

    change eng to kor

--- lib/Resource/Strings.dart
+++ lib/Resource/Strings.dart
@@ -2,8 +2,8 @@
 class Strings {
   Strings._();
 
-  static const String AT_LEAST = "At least - character";
-  static const String UPPERCASE_LETTER = "- Uppercase letter";
-  static const String NUMERIC_CHARACTER = "- Numeric character";
-  static const String SPECIAL_CHARACTER = "- Special character";
+  static const String AT_LEAST = "최소 -글자 이상";
+  static const String UPPERCASE_LETTER = "최소 -개 이상 대문자 포함";
+  static const String NUMERIC_CHARACTER = "최소 -개 이상 숫자 포함";
+  static const String SPECIAL_CHARACTER = "최소 -개 이상 특수문자 포함";
 }
